af13ee39b11775ea1081d7e6af100b2cf9b064fc
Ibrahim Tayib <ibrahimtayib@Ibrahims-MacBook-Pro.local>
2021-04-08 00:00:43 +1000

    Included comments

--- dev/api.js
+++ dev/api.js
@@ -5,23 +5,37 @@ var app = express();
 // Declare port number
 var portNumber = 3001;
 
-// GET: Entire blockchain
+/**
+ * GET: Return entire blockchain
+ * @returns {Array}
+ */
+
 app.get('/blockchain', (request, response) => {
 
 });
 
-// POST: A new transaction
+/**
+ * POST: New Transaction
+ * @returns {Array}  
+ */
+
 app.post('/transaction', (request, response) => {
     console.log(req.body);
     response.send(`The amount of the transaction is ${req.body.amount} bitcoin`);
 });
 
-// GET: Mine a new block
+/**
+ * GET: Mine new block
+ * @returns {Array}
+ */
 app.get('/mine', (request, response) => {
 
 });
 
-// Listen to port
+/**
+ * Create server and listen to a communitcation port 
+ * @listens portNumber
+ */
 app.listen(portNumber, () => {
     console.log(`Listening on port ${portNumber}`);
     
